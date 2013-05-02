4adf0c742f0cb17311252ce693eddaf467aa2f0f
Julianne Beaudoin <jubeaudoin@gmail.com>
2013-05-02 14:37:34 -0700

    first commit rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestBus::Application.config.secret_token = '00ca638c00d00cf2303af5c6f68f65158ac1dbebe34d30f3810cf8d44341d52f458820d8717470cdb9c7789eecae511c082a8ed3ba53c700744091c694892641'
