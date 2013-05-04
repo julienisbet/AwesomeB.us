532c078f3268bf4e0603132a7fc5b3998e5dc58e
Julianne Beaudoin <jubeaudoin@gmail.com>
2013-05-04 15:55:16 -0700

    added countdown functions to countdown.js

---
+++ app/assets/javascripts/countdown.js
@@ -0,0 +1,33 @@
+var timer;
+var totalSeconds;
+
+function CreateTimer(timerID, time) {
+  timer = document.getElementById(timerID);
+  totalSeconds = time;
+
+  UpdateTimer();
+  window.setTimeout("Tick()", 1000);
+}
+
+function Tick() {
+  if (totalSeconds <= 0) {
+    alert("GTFO!");
+    return;
+  }
+  totalSeconds -= 1;
+  UpdateTimer();
+  window.setTimeout("Tick()", 1000);
+}
+
+function UpdateTimer() {
+  var seconds = totalSeconds;
+  var minutes = Math.floor(seconds / 60);
+  var timeStr = (LeadingZero(minutes) + " : " + LeadingZero(seconds));
+  timer.innerHTML = timeStr;
+}
+
+function LeadingZero(time) {
+  return (time < 10) ? "0" + time : + time;
+}
+
+CreateTimer("timer", 30);
