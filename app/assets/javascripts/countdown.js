e1967d4f4faf68bef929fc2ec18fb0c193c9eabc
Julie Nisbet <jmnisbet@gmail.com>
2013-05-05 16:41:07 -0700

    display timers on main page

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -2,13 +2,16 @@ var timer;
 var totalSeconds;
 
 function drawTimer(element,color,text,time){
+      var c = $("#" + element)
+      var container = $(c).parent();
+      c.attr('width', $(container).height() ); //max width
+      c.attr('height', $(container).height() ); //max height
+
       var canvas = document.getElementById(element);
       var context = canvas.getContext('2d');
       var x = canvas.width / 2;
       var y = canvas.height / 2;
-      var radius = Math.min(x,y)*0.90;
-      debugger
-
+      var radius = Math.min(x,y)*0.70;
       var start_angle = 3/2*Math.PI;
       var time_angle = 2*Math.PI / 60 * time;
         //clear previous data
@@ -19,22 +22,22 @@ function drawTimer(element,color,text,time){
         
         context.beginPath();
         context.strokeStyle=color;
-        context.lineWidth=20;
+        context.lineWidth=(Math.min(canvas.width, canvas.height)*0.05);
         context.arc(x,y,radius,start_angle,start_angle + time_angle,false);
         context.stroke();
         context.closePath();
 
         var time_text = time.toString();
         context.fillStyle = "white"; // font color to write the text with
-        var font = "bold " + 45 +"px serif";
+        var font = "bold " + (Math.min(canvas.width, canvas.height)*0.25) +"px serif";
         context.font = font;
         // Move it down by half the text height and left by half the text width
         var width = context.measureText(time_text).width;
-        var height_num = context.measureText("w").width; // this is a GUESS of height
+        var height_num = context.measureText("w").width - 5; // this is a GUESS of height
         context.fillText(time_text, x - (width/2) ,y + (height_num/2));
         
         context.fillStyle = color; // font color to write the text with
-        var font = "bold " + 15 +"px serif";
+        var font = "bold " + (Math.min(canvas.width, canvas.height)*0.10) +"px serif";
         context.font = font;
         // Move it down by half the text height and left by half the text width
         var width = context.measureText(text).width;
@@ -44,12 +47,11 @@ function drawTimer(element,color,text,time){
 
 
 function displayTimer(total_seconds) {
-  debugger
       var minutes = Math.floor(total_seconds / 60);      
       var seconds = total_seconds - minutes*60;
 
-      drawTimer('top-box1',"green","MINUTES",minutes--)
-      drawTimer('top-box2',"yellow","SECONDS",seconds)
+      drawTimer('minutesTimer',"green","MINUTES",minutes--)
+      drawTimer('secondsTimer',"yellow","SECONDS",seconds)
 
       var timer = setInterval(function() { 
         if (seconds==-1) {
@@ -59,13 +61,17 @@ function displayTimer(total_seconds) {
           }
           else {
             seconds=59
-            drawTimer('top-box1',"green","MINUTES",minutes--)
+            drawTimer('minutesTimer',"green","MINUTES",minutes--)
 
           }
         };
-      drawTimer('top-box2',"yellow","SECONDS",seconds--)
-      }, 250)
+      drawTimer('secondsTimer',"yellow","SECONDS",seconds--)
+      }, 1000)
 }
-  
 
-displayTimer(75);
+displayTimer(1000)
+
+
+
+
+
