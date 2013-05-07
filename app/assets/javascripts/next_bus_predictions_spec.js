3826120e4f6f887e9110a10341eb1e9a33e5051f
Eric Dykstra <eric@ericdykstra.me>
2013-05-07 12:06:11 -0700

    removed random spec js from assets

--- app/assets/javascripts/next_bus_predictions_spec.js
+++
@@ -1,6 +0,0 @@
-describe("The 'nextBusPredictions' function", function() {
-  it("should return a URL in a format NextBus can understand with inputs given", function() {
-    x = nextBusPredictions('22','stoptagname');
-    expect(x).toEqual("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=22&s=stoptagname");
-  });
-});
