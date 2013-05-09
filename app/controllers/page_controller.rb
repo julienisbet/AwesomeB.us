634a71625d3949979b19a883d6b2bfab80b20b1a
Eric Dykstra <eric@ericdykstra.me>
2013-05-09 12:07:29 -0700

    took away buffer time, still multiple destinations available on page if you look carefully

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -7,12 +7,8 @@ class PageController < ApplicationController
       history[params[:destination]] += 1 if history[params[:destination]]
       history.merge!({params[:destination] => 1}) if not history[params[:destination]]
     end
-    if params[:buffer]
-      cookies[:buffer] = params[:buffer]
-    end
     cookies[:history] = ActiveSupport::JSON.encode(history)
     @destinations = history.sort_by{|k,v| v}.reverse.map{|k,v| k}
-    @buffer = cookies[:buffer]
   end
 
 end
